9d88ae5b4d7ad04934b056b4307288bcf21d9463
Michael Mehrtens <michael.david.mehrtens@gmail.com>
2021-12-02 13:24:22 -0600

    Add second star challenge with aim

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,6 +1,15 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+/// Open an input path and return a buffered reader over the contents.
+fn get_buf_reader(input_path: &str) -> BufReader<File> {
+    // Create a buffer to read the file line by line
+    let contents =
+        File::open(input_path).expect(format!("Error reading file: {}", input_path).as_str());
+    let reader = BufReader::new(contents);
+    reader
+}
+
 /// Record movements of forward, up, and down to retrieve the final (horizontal, depth) coordinates of the movements.
 ///
 /// # Arguments
@@ -29,10 +38,7 @@ use std::io::{BufRead, BufReader};
 /// Would produce a final position of (15, 10).
 ///
 fn record_movements(input_path: &str) -> (i32, i32) {
-    // Create a buffer to read the file line by line
-    let contents =
-        File::open(input_path).expect(format!("Error reading file: {}", input_path).as_str());
-    let reader = BufReader::new(contents);
+    let reader = get_buf_reader(input_path);
     let (mut horizontal, mut depth) = (0, 0);
     for line in reader.lines() {
         let line = line.expect("Failed to parse line from file.");
@@ -50,6 +56,70 @@ fn record_movements(input_path: &str) -> (i32, i32) {
             "forward" => horizontal += score,
             "up" => depth -= score,
             "down" => depth += score,
+            _ => panic!("Unknown direction: {}", line),
+        }
+    }
+    (horizontal, depth)
+}
+
+/// Record movements of forward, up, and down to retrieve the final (horizontal, depth) coordinates of the movements.
+///
+/// Records movements using *aim* concept, where rather than simply changing directions, up/down movements just adjust
+/// an aim factor, with only forward movements impacting depth.
+///
+/// * down X increases aim by X units.
+/// * up X decreases aim by X units.
+/// * forward X does two things:
+///   * It increases horizontal position by X units.
+///   * It increases depth by your aim multiplied by X.
+///
+/// # Arguments
+///
+/// * `input_path - The input file path containing the movements
+///
+/// # Returns
+///
+/// The (horizontal, depth) coordinates of the final position.
+///
+/// # Examples
+///
+/// ## Basic
+///
+/// For example, the movements:
+///
+/// ```
+/// forward 5
+/// down 5
+/// forward 8
+/// up 3
+/// down 8
+/// forward 2
+/// ```
+///
+/// Would produce a final position of (15, 60).
+///
+fn record_movements_with_aim(input_path: &str) -> (i32, i32) {
+    let reader = get_buf_reader(input_path);
+    let (mut horizontal, mut depth, mut aim) = (0, 0, 0);
+    for line in reader.lines() {
+        let line = line.expect("Failed to parse line from file.");
+        let mut parts: Vec<&str> = line.split(" ").collect();
+        if parts.len() != 2 {
+            panic!("Got unreadable line: {}", line);
+        }
+        let score = parts
+            .pop()
+            .unwrap()
+            .parse::<i32>()
+            .expect("Failed to parse movement size.");
+        let key = parts.pop().unwrap();
+        match key {
+            "forward" => {
+                depth += aim * score;
+                horizontal += score;
+            }
+            "up" => aim -= score,
+            "down" => aim += score,
             _ => panic!("Unknown direction: {}", line),
         }
     }
@@ -88,7 +158,20 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let input_path = parse_file_path(&args);
     let (x, y) = record_movements(input_path);
-    println!("Final coordinates: ({}, {}), multiplied: {}", x, y, x * y);
+    println!(
+        "Final coordinates no aim: ({}, {}), multiplied: {}",
+        x,
+        y,
+        x * y
+    );
+
+    let (x, y) = record_movements_with_aim(input_path);
+    println!(
+        "Final coordinates with aim: ({}, {}), multiplied: {}",
+        x,
+        y,
+        x * y
+    );
 }
 
 #[cfg(test)]
@@ -123,6 +206,22 @@ mod test_parse_file_path {
 }
 
 #[cfg(test)]
+mod test_get_buf_reader {
+    use crate::get_buf_reader;
+
+    #[test]
+    #[should_panic]
+    fn error_file_handled() {
+        get_buf_reader("inputs/noexist.txt");
+    }
+
+    #[test]
+    fn example_file_handled() {
+        get_buf_reader("inputs/example.txt");
+    }
+}
+
+#[cfg(test)]
 mod test_record_movements {
     use crate::record_movements;
 
@@ -132,13 +231,25 @@ mod test_record_movements {
     }
 
     #[test]
-    fn question_correct_small_window() {
+    fn question_correct() {
         assert_eq!(record_movements("inputs/challenge.txt"), (1845, 916));
+    }
+}
+
+#[cfg(test)]
+mod test_record_movements_with_aim {
+    use crate::record_movements_with_aim;
+
+    #[test]
+    fn example_correct() {
+        assert_eq!(record_movements_with_aim("inputs/example.txt"), (15, 60));
     }
 
     #[test]
-    #[should_panic]
-    fn error_file_handled() {
-        record_movements("inputs/noexist.txt");
+    fn question_correct() {
+        assert_eq!(
+            record_movements_with_aim("inputs/challenge.txt"),
+            (1845, 763408)
+        );
     }
 }

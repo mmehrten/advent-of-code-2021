fbb7c26f45a7c44635e9bee84bb342114cd2cbd7
Michael Mehrtens <michael.david.mehrtens@gmail.com>
2021-12-05 18:08:51 -0600

    Add template AOC solution

---
+++ template/src/main.rs
@@ -0,0 +1,133 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+/// Parse the file path from command line arguments.
+///
+/// # Arguments
+///
+/// * `args` - the command line arguments
+///
+/// # Returns
+///
+/// A single command line argument - panics if zero or more than one argument are passed.
+fn parse_file_path(args: &[String]) -> &str {
+    if args.len() != 2 {
+        panic!(
+            "Expected one file path and an optional window size to run against, got: {} arguments",
+            args.len() - 1
+        );
+    }
+    let input_path = &args[1];
+    input_path.as_str()
+}
+
+#[cfg(test)]
+mod test_parse_file_path {
+    use crate::parse_file_path;
+
+    #[test]
+    fn one_arg_ok() {
+        assert_eq!(
+            parse_file_path(&vec!["script_path".to_string(), "arg_text".to_string()][..]),
+            "arg_text"
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn no_arg_fail() {
+        parse_file_path(&Vec::new());
+    }
+
+    #[test]
+    #[should_panic]
+    fn many_arg_fail() {
+        parse_file_path(
+            &vec![
+                "script_path".to_string(),
+                "arg_text".to_string(),
+                "extra_arg".to_string(),
+            ][..],
+        );
+    }
+}
+
+/// Open an input path and return a buffered reader over the contents.
+fn get_buf_reader(input_path: &str) -> BufReader<File> {
+    // Create a buffer to read the file line by line
+    let contents =
+        File::open(input_path).expect(format!("Error reading file: {}", input_path).as_str());
+    let reader = BufReader::new(contents);
+    reader
+}
+
+#[cfg(test)]
+mod test_get_buf_reader {
+    use crate::get_buf_reader;
+
+    #[test]
+    #[should_panic]
+    fn error_file_handled() {
+        get_buf_reader("inputs/noexist.txt");
+    }
+
+    #[test]
+    fn example_file_handled() {
+        get_buf_reader("inputs/example.txt");
+    }
+}
+
+/// TODO
+///
+/// # Arguments
+///
+/// * `input_path - The input file path TODO.
+///
+/// # Returns
+///
+/// TODO.
+///
+/// # Examples
+///
+/// ## Basic
+/// 
+/// TODO
+fn solution(input_path: &str) -> (i32, i32) {
+    let reader = get_buf_reader(input_path);
+    let lines = reader.lines();
+    let calls = lines.next();
+    for line in lines {
+        let line = line.expect("Failed to parse line from file.");
+    }
+    (0, 0)
+}
+
+/// TODO
+///
+/// Usage:
+///
+/// ```
+/// $ day-TODO inputs/example.txt
+/// TODO
+/// ```
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let input_path = parse_file_path(&args);
+    let sol = solution(input_path);
+    println!("TODO: {:?}", sol);
+}
+
+#[cfg(test)]
+mod test_solution {
+    use crate::solution;
+
+    #[test]
+    fn example_correct() {
+        assert_eq!(solution("inputs/example.txt"), (0, 0));
+    }
+
+    #[test]
+    fn question_correct() {
+        assert_eq!(solution("inputs/challenge.txt"), (0, 0));
+    }
+}
